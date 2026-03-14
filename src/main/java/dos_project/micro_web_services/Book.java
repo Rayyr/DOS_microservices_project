233eb811aed3dd82e0334b7709e55375bfa8885c
raya <khasaatirayaa@gmail.com>
2026-03-14 06:59:10 +0200

    rename book-routing name , make the entire api start from main func

--- src/main/java/dos_project/micro_web_services/Book.java
+++ src/main/java/dos_project/micro_web_services/Book.java
@@ -6,13 +6,14 @@ import com.google.gson.Gson;
 import java.sql.*;
 import java.util.*;
 
-public class test2 {
+public class Book {
 
     static List<Map<String, Object>> books = new ArrayList<>();
 
-	public static void main(String[] args) {
+	public static void routing() {
 
 		getData();
+		//GET action 
 		get("/getBooks/all", (req, res) -> {
 			
 			res.type("application/json");    // response type
