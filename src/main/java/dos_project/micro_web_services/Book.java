dc5b1d99370e4db36f852d12d3da539cc273d153
raya <khasaatirayaa@gmail.com>
2026-03-14 10:08:40 +0200

    add 3rd endpoint : info/id

--- src/main/java/dos_project/micro_web_services/Book.java
+++ src/main/java/dos_project/micro_web_services/Book.java
@@ -22,7 +22,9 @@ public class Book {
 		
 		
 		
-		//GET action for specific book based to specific topic by passing it via path params of the request
+		
+		
+		//GET action for specific books based to specific topic by passing it via path params of the request
 		get("/search/:topic",(req,res)->{
 			String topic=req.params(":topic");//extract topic from the URL
 			books=getBooksBasedToTopic(topic);
@@ -31,8 +33,55 @@ public class Book {
 			return new Gson().toJson(books); // convert to JSON
 		});
 	 
+		
+		
+		
+		
+		
+		//GET action for specific book information based to specific id by passing it via path params of the request
+		get("/info/:id",(req,res)->{
+			int id=Integer.parseInt(req.params(":id"));//extract id from the URL
+			books=getBookInfoBasedToID(id);
+			if(books.size()==0) return "There is no book associated with the specified ID : "+id;
+			res.type("application/json");    // response type
+			return new Gson().toJson(books); // convert to JSON
+		});
+		
+		
 	}
 
+	
+	
+	
+	
+	
+	private static List<Map<String, Object>> getBookInfoBasedToID(int id) {
+		
+		List<Map<String, Object>> allBooks=new ArrayList<>();
+		try ( 
+			     Statement statement = Api.dbCon.createStatement();
+			     ResultSet rs = statement.executeQuery("SELECT * from Book where book_id="+id)) {
+
+			ResultSetMetaData meta = rs.getMetaData();
+			  while (rs.next()) {
+                  Map<String, Object> book = new LinkedHashMap<>(); //we use linkedhashmap tp preserve the insertion order 
+                  book.put(meta.getColumnName(2), rs.getString("title"));
+                  book.put(meta.getColumnName(3), rs.getString("description"));
+                  book.put(meta.getColumnName(4), rs.getDouble("cost"));
+                  book.put(meta.getColumnName(5), rs.getInt("quantity"));
+                  book.put(meta.getColumnName(6), rs.getString("topic"));
+                  allBooks.add(book);
+              }
+
+			} catch (SQLException e) {
+				System.err.print("Sorry , there is an error with data base : "+e.getMessage());
+			}
+		return allBooks;
+	}
+	
+	
+	
+	
 	
 	private static List<Map<String, Object>> getBooksBasedToTopic(String topic) {
 		
