181f04f0f7c88384c6875e0bcef94a529ad656f8
raya <khasaatirayaa@gmail.com>
2026-03-14 08:58:57 +0200

    add 2nd endpoint : search/topic

--- src/main/java/dos_project/micro_web_services/Book.java
+++ src/main/java/dos_project/micro_web_services/Book.java
@@ -8,18 +8,60 @@ import java.util.*;
 
 public class Book {
  
+	static List<Map<String, Object>> books;
 	public static void routing( ) {
 		 
 		//GET action for all books  
 		get("/getBooks/all", (req, res) -> {
-			List<Map<String, Object>> allBooks=getAllBooks();
-			if(allBooks.size()==0) return "there is no books in the stock!";
+			books=getAllBooks();
+			if(books.size()==0) return "There is no books in the stock!";
 			res.type("application/json");    // response type
-			return new Gson().toJson(getAllBooks()); // convert to JSON
+			return new Gson().toJson(books); // convert to JSON
+		});
+		
+		
+		
+		
+		//GET action for specific book based to specific topic by passing it via path params of the request
+		get("/search/:topic",(req,res)->{
+			String topic=req.params(":topic");//extract topic from the URL
+			books=getBooksBasedToTopic(topic);
+			if(books.size()==0) return "There is no matching books with the specified topic : "+topic;
+			res.type("application/json");    // response type
+			return new Gson().toJson(books); // convert to JSON
 		});
 	 
 	}
 
+	
+	private static List<Map<String, Object>> getBooksBasedToTopic(String topic) {
+		
+		List<Map<String, Object>> allBooks=new ArrayList<>();
+		try ( 
+			     Statement statement = Api.dbCon.createStatement();
+			     ResultSet rs = statement.executeQuery("SELECT book_id,title FROM Book where topic=\'"+topic+"\'")) {
+
+			ResultSetMetaData meta = rs.getMetaData();
+			  while (rs.next()) {
+                  Map<String, Object> book = new LinkedHashMap<>(); //we use linkedhashmap tp preserve the insertion order 
+                  book.put(meta.getColumnName(1), rs.getInt("book_id"));
+                  book.put(meta.getColumnName(2), rs.getString("title"));
+                /*  book.put(meta.getColumnName(3), rs.getString("description"));
+                  book.put(meta.getColumnName(4), rs.getDouble("cost"));
+                  book.put(meta.getColumnName(5), rs.getInt("quantity"));
+                  book.put(meta.getColumnName(6), rs.getString("topic"));*/
+                  allBooks.add(book);
+              }
+
+			} catch (SQLException e) {
+				System.err.print("Sorry , there is an error with data base : "+e.getMessage());
+			}
+		return allBooks;
+	}
+	
+	
+	
+	
 	private static List<Map<String, Object>> getAllBooks() {
 	
 		List<Map<String, Object>> allBooks=new ArrayList<>();
@@ -40,7 +82,7 @@ public class Book {
               }
 
 			} catch (SQLException e) {
-				System.err.print("Sorry , there is an error data base connection : "+e.getMessage());
+				System.err.print("Sorry , there is an error with data base : "+e.getMessage());
 			}
 		return allBooks;
 	}
